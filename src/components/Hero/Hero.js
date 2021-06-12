3010b4928369d19a161590dc6b8c5a099593c90e
stanley <lancylotdsouza@gmail.com>
2021-06-12 09:55:18 +0530

    learn more button updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
-
+const handleClick = (e, i) => {
+  e.preventDefault();
+  window.location.href= '#about';
+}
 const Hero = (props) => (
-  <>
-    <Section row nopadding>
-      <LeftSection>
+  < >
+    <Section row nopadding >
+      <LeftSection >
         <SectionTitle main center>
           Welcome To <br />
           My Personal Portfolio
@@ -15,7 +18,7 @@ const Hero = (props) => (
         <SectionText>
             Building quality websites and applications with Laravel, Vue Js, React Js, Codeigniter and Wordpress.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick={(e) => handleClick(e)} >Learn More</Button>
       </LeftSection>
     </Section>
   </>
